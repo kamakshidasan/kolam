1da4061865fba3015a2c026e9a9d4a555f5e755c
kamakshidasan <adhitya.kamakshidasan@gmail.com>
2018-11-30 01:47:51 +0100

    initial commit

---
+++ create-scalar-field.py
@@ -0,0 +1,11 @@
+import pylab as p
+import numpy as np
+import mahotas
+import cv2
+
+im = cv2.imread("flower.png", cv2.IMREAD_GRAYSCALE)
+dmap = mahotas.distance(im)
+p.imshow(dmap, cmap="coolwarm")
+#p.axis('off')
+#p.savefig('scalar.png', bbox_inches='tight', pad_inches=0)
+p.show()
---
+++ segmentation.py
@@ -0,0 +1,25 @@
+import mahotas
+import numpy as np
+from matplotlib import pyplot as plt
+import random
+from matplotlib import colors as c
+
+nuclear = mahotas.imread('flower.png')
+nuclear = nuclear[:,:,0]
+nuclear = mahotas.gaussian_filter(nuclear, 1.)
+threshed  = (nuclear > nuclear.mean())
+distances = mahotas.stretch(mahotas.distance(threshed))
+Bc = np.ones((9,9))
+
+maxima = mahotas.morph.regmax(distances, Bc=Bc)
+spots,n_spots = mahotas.label(maxima, Bc=Bc)
+surface = (distances.max() - distances)
+areas = mahotas.cwatershed(surface, spots)
+areas *= threshed
+
+plt.jet()
+
+rmap = c.ListedColormap(np.random.rand(256,3))
+
+plt.imshow(areas, cmap=rmap)
+plt.show()
